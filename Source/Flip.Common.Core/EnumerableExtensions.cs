a1a86acd85761233b80d6b675165f24cea40637f
Jesper <jesper.o.astrom@gmail.com>
2012-11-12 10:04:35 +0100

    Added string extensions to Core.

---
+++ Source/Flip.Common.Core/EnumerableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Flip.Common
+{
+	public static class EnumerableExtensions
+	{
+		public static string ToSeparatedString<T>(this IEnumerable<T> source, Func<T, string> predicate, string separator)
+		{
+			StringBuilder builder = new StringBuilder();
+			foreach (T item in source)
+			{
+				string s = predicate(item);
+				if (!string.IsNullOrEmpty(s))
+				{
+					builder.Append(s);
+					builder.Append(separator);
+				}
+			}
+			if (builder.Length > separator.Length)
+			{
+				builder.Remove(builder.Length - separator.Length, separator.Length);
+			}
+			return builder.ToString();
+		}
+
+		public static void ForEachWithIndex<T>(this IEnumerable<T> source, Action<int, T> action)
+		{
+			int index = 0;
+			foreach (T item in source)
+			{
+				action(index, item);
+				index++;
+			}
+		}
+	}
+}
